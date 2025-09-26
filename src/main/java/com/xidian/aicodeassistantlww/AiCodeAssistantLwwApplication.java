3ba5a32940c8752e1bf07ea4be9e9f2f6e403c26
git@github.com:arousa1/Ai-Code-Gen.git <496177401@qq.com>
2025-09-26 15:15:10 +0800

    Ai应用代码生成器用户模块 1.使用Mybatis-flex代码生成器生成代码 2.用户常见接口开发

--- src/main/java/com/xidian/aicodeassistantlww/AiCodeAssistantLwwApplication.java
+++ src/main/java/com/xidian/aicodeassistantlww/AiCodeAssistantLwwApplication.java
@@ -1,11 +1,14 @@
 package com.xidian.aicodeassistantlww;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 
 @EnableAspectJAutoProxy(exposeProxy = true)
 @SpringBootApplication
+@MapperScan("com.xidian.aicodeassistantlww.mapper")
 public class AiCodeAssistantLwwApplication {
 
     public static void main(String[] args) {
---
+++ src/main/java/com/xidian/aicodeassistantlww/generator/MyBatisCodeGen.java
@@ -0,0 +1,75 @@
+package com.xidian.aicodeassistantlww.generator;
+
+import cn.hutool.core.lang.Dict;
+import cn.hutool.setting.yaml.YamlUtil;
+import com.mybatisflex.codegen.Generator;
+import com.mybatisflex.codegen.config.ColumnConfig;
+import com.mybatisflex.codegen.config.GlobalConfig;
+import com.zaxxer.hikari.HikariDataSource;
+
+import java.util.Map;
+
+public class MyBatisCodeGen {
+
+    // 需要生成代码的表名
+    private static final String[] TABLE_NAMES = {"user"};
+
+    public static void main(String[] args) {
+        //加载配置文件
+        Dict dict = YamlUtil.loadByPath("application.yml");
+        Map<String, Object> databaseConfig = dict.getByPath("spring.datasource");
+        String url = String.valueOf(databaseConfig.get("url"));
+        String username = String.valueOf(databaseConfig.get("username"));
+        String password = String.valueOf(databaseConfig.get("password"));
+        //配置数据源
+        HikariDataSource dataSource = new HikariDataSource();
+        dataSource.setJdbcUrl(url);
+        dataSource.setUsername(username);
+        dataSource.setPassword(password);
+
+        //创建配置内容，两种风格都可以。
+        GlobalConfig globalConfig = createGlobalConfig();
+
+        //通过 datasource 和 globalConfig 创建代码生成器
+        Generator generator = new Generator(dataSource, globalConfig);
+
+        //生成代码
+        generator.generate();
+    }
+
+    public static GlobalConfig createGlobalConfig() {
+        //创建配置内容
+        GlobalConfig globalConfig = new GlobalConfig();
+
+        //设置根包
+        globalConfig.getPackageConfig()
+                .setBasePackage("com.xidian.aicodeassistantlww.genresult");
+
+        //设置表前缀和只生成哪些表，setGenerateTable 未配置时，生成所有表
+        globalConfig.getStrategyConfig()
+                .setGenerateTable(TABLE_NAMES)
+                .setLogicDeleteColumn("isDelete"); // 设置逻辑删除字段
+
+        //设置生成 entity 并启用 Lombok
+        globalConfig.enableEntity()
+                .setWithLombok(true)
+                .setJdkVersion(21);
+
+        //设置生成 mapper
+        globalConfig.enableMapper();
+        globalConfig.enableMapperXml();
+
+        // 设置生成 service
+        globalConfig.enableService();
+        globalConfig.enableServiceImpl();
+
+        // 设置生成 controller
+        globalConfig.enableController();
+
+        // 设置生成时间和字符串为空，避免多余的代码改动
+        globalConfig.getJavadocConfig()
+                .setAuthor("lww")
+                .setSince("");
+        return globalConfig;
+    }
+}
